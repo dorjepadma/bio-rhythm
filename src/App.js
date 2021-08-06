698e8758037bacbb5e1d7ac9e3f95a70abe96f09
dorjepadma <dorjepadma@gmail.com>
2021-08-05 23:27:28 -0700

    added Ionic Card

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import {
   IonItem,
   IonLabel,
   IonDatetime,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
 } from '@ionic/react';
 import React, { useState } from 'react';
 
@@ -32,6 +36,18 @@ function App() {
         <IonDatetime displayFormat='D MMM YYYY' value={birthDate}
         onIonChange={(event)=> setBirthDate(event.detail.value)}/>
         </IonItem>
+        <IonCard>
+          <IonCardHeader className='ion-text-center'>
+            <IonCardTitle>Your Biorhythm Today:</IonCardTitle>
+            <IonCardTitle>5-Jan-2021</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent className='ion-text-center'>
+          
+            <p>Physical: 83%</p>
+            <p>Emotional: 83%</p>
+            <p>Mental: 83%</p>
+          </IonCardContent>
+        </IonCard>
         <p>Name: {name}</p>
         <p>Date of Birth: {birthDate}</p>
       </IonContent>
